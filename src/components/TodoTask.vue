9d2c073e395d0cf89fd33a784f5dbfac4796c0c0
HalLuckcinate <108384305+HalLuckcinate@users.noreply.github.com>
2022-07-05 08:43:34 +0700

    Message

--- src/components/TodoTask.vue
+++ src/components/TodoTask.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="flex text-center p-4 m-2">
-    <p class="flex-1">{{ name.id }}</p>
-    <p class="flex-1 text-black">{{ name.name }}</p>
-    <p class="flex-1">{{ name.status }}</p>
-    <button class="flex-1 bg-yellow-500 text-center text-black rounded-md">
+  <div class="flex text-center p-4 m-2 text-black">
+    <p class="flex-1">{{ id }}</p>
+    <p class="flex-1">{{ name }}</p>
+    <p class="flex-1">{{ status }}</p>
+    <button
+      @click="$emit('edit')"
+      class="flex-1 bg-yellow-500 text-center text-black rounded-md"
+    >
       Edit
     </button>
-    
-    <button class="flex-1 bg-red-500 text-center text-black rounded-md">
+
+    <button
+      @click="$emit('delete')"
+      class="flex-1 bg-red-500 text-center text-black rounded-md"
+    >
       Delete
     </button>
   </div>
@@ -15,10 +21,18 @@
 
 <script setup>
 defineProps({
+  id: {
+    type: String,
+  },
   name: {
-    type: Object,
+    type: String,
+  },
+  status: {
+    type: String,
   },
 });
+
+defineEmits(["delete", "edit"]);
 </script>
 
 <style></style>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -3,16 +3,26 @@
     <div
       class="bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg text-left text-white p-4 flex"
     >
-      <p class="text-left flex-auto">Todo List App for newbie</p>
-      <a-button
-        class="float-right bg-gradient-to-r from-green-500 to-blue-600 flex-auto rounded-full py-2"
-        type="primary"
-        @click="showModal"
-      >
-        Add new Task
+      <p class="text-left flex-auto text-2xl font-bold">
+        Todo List App for newbie
+      </p>
+      <a-button class="btn-add" type="primary" @click="showModal">
+        Add New Task
       </a-button>
-      <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
-        <p>Some contents...</p>
+      <a-modal
+        v-model:visible="visible"
+        title="Insert tasks information"
+        @ok="handleOk"
+        class=""
+      >
+        <div class="text-xl font-bold">ID</div>
+        <a-input class="input" v-model:value="idTodo" />
+
+        <div class="text-xl font-bold">Task Name</div>
+        <a-input class="input" v-model:value="nameTodo" id="new-nameTodo" />
+
+        <div class="text-xl font-bold">Status</div>
+        <a-input class="input" v-model:value="statusTodo" id="new-statusTodo" />
       </a-modal>
     </div>
 
@@ -24,7 +34,15 @@
       <p class="flex-1">Delete</p>
     </div>
 
-    <todo-task :name="todo" />
+    <todo-task
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      :id="todo.id"
+      :name="todo.name"
+      :status="todo.status"
+      
+      @delete="todos.splice(index, 1)"
+    />
   </div>
 </template>
 
@@ -32,17 +50,62 @@
 import TodoTask from "../components/TodoTask.vue";
 import { defineComponent, ref } from "vue";
 
-const todo = ref({ id: "1", name: "task1", status: "Processing" });
+const todos = ref([
+  { id: "1", name: "task1", status: "Processing" },
+  { id: "2", name: "task2", status: "Processing" },
+  { id: "3", name: "task3", status: "Todo" },
+  { id: "4", name: "task4", status: "Done" },
+]);
+
 const visible = ref<boolean>(false);
 
 const showModal = () => {
   visible.value = true;
+
+  idTodo.value = "";
+  nameTodo.value = "";
+  statusTodo.value = "";
 };
 
 const handleOk = (e: any) => {
   console.log(e);
   visible.value = false;
+
+  addNew();
 };
+
+const nameTodo = ref("");
+const statusTodo = ref("");
+const idTodo = ref("");
+
+function addNew() {
+  console.log(1111);
+
+  todos.value.push({
+    id: idTodo.value,
+    name: nameTodo.value,
+    status: statusTodo.value,
+  });
+  // nameTodo.value = "";
+  // statusTodo.value = "";
+}
 </script>
 
-<style></style>
+<style>
+.btn-add {
+  background-image: linear-gradient(to right, blue, rgb(0, 255, 157));
+  font-size: 18px;
+  font-weight: 400;
+}
+
+.input {
+  background-color: #f4f4f4;
+  font-size: 20px;
+  border: 3px solid;
+  border-top-color: blue;
+  border-left-color: blue;
+  border-bottom-color: blueviolet;
+  border-right-color: blueviolet;
+  padding-block: 5px;
+}
+</style>
